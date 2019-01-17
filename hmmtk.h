38611b6b88909ebd3ef91391f8b0dc4c685b43b5
Gijsbert Voshol <gpvoshol@Gijsberts-MacBook-Pro.local>
2019-01-17 09:38:12 +0100

    Fix chmm_outprob() again

--- hmmtk.h
+++ hmmtk.h
@@ -2146,14 +2146,16 @@ void chmm_outprob(CHMM *hmm, double **sample, int T, double **outprob)
 	double **cov = hmm->B.cov; // covariance
 	double **cov_inv = hmm->B.cov_inv;
 
-	double prob1 = - 0.5 * D * eln(2.0 * M_PI); // -N*log(2pi)/2
-	double *prob2 = (double*) dvector(1, N);  // -1/2*log(|sigma|)
+	// logP(x)=−2ln(2π)− lnσi − 1/2 (xi −μi)/σi2
+
+	double prob1 = - 0.5 * D * eln(2.0 * M_PI); // -−2ln(2π)
+	double *prob2 = (double*) dvector(1, N);  // − lnσi
 	for (i = 1; i <= N; i++) {
 		double tmp = 0.0;
 		for (j = 1; j <= D; j++) {
-			tmp += eln(cov[i][j]);
+			tmp += eln(sqrt(cov[i][j]));
 		}
-		prob2[i] = - tmp * 0.5;
+		prob2[i] = - tmp;
 	}
 
 	for (f = 0; f < T; f++) {
@@ -2163,8 +2165,10 @@ void chmm_outprob(CHMM *hmm, double **sample, int T, double **outprob)
 			tmp = 0.0;
 			for (j = 1; j <= D; j++) {
 				x = feature_tmp[j - 1] - miu[i][j];
+				// (xi −μi)/σi2
 				tmp += x * x * cov_inv[i][j];
 			}
+			// logP(x)=−2ln(2π)− lnσi − 1/2 (xi −μi)/σi2
 			outprob[i][f + 1] = prob1 + prob2[i] - 0.5 * tmp;
 		}
 	}
